1b7c7c1fe276eeeac28b0ee6ab5059d5c3c61e4d
Piphvt <Piphat.Upachatai@gmail.com>
2025-08-23 04:35:00 +0700

    test thumnails 1.3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,33 @@ const outfit = Outfit({
   variable: '--font-outfit',
 });
 
+const baseUrl = "https://piphat-portfolio.vercel.app";
+
 export const metadata = {
   title: 'Piphat Portfolio',
-  description: 'Created by Piphat Upachatai',
+  description: 'A developer who turns ideas into interactive experiences.',
+  openGraph: {
+    title: 'Piphat Portfolio',
+    description: 'A developer who turns ideas into interactive experiences.',
+    url: baseUrl,
+    siteName: 'Piphat Portfolio',
+    images: [
+      {
+        url: `${baseUrl}/image/og-image.png`,
+        width: 1200,
+        height: 630,
+        alt: 'Piphat Portfolio Thumbnail',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Piphat Portfolio',
+    description: 'A developer who turns ideas into interactive experiences.',
+    images: [`${baseUrl}/image/og-image.png`],
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
